1dd45760911955378600fd6f9c12b5526fdc797a
Pablo Gallina <pablogallinag@gmail.com>
2021-12-15 14:15:07 -0600

    Formulario login (sin estilos)

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import GoogleButton from 'react-google-button'
+
+import { Link } from 'react-router-dom'
+
 const LoginScreen = () => {
     return (
         <>
-            <h1>Login</h1>   
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder='user'/>
+                <input type="password" name="" id="" />
+                <button type="submit">Login</button>
+            </form>   
+            <GoogleButton onClick={()=> console.log("google")} />
+            <Link to="/register">Registrarse</Link>
+
         </>
     )
 }
