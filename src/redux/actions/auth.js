6ba36c15bba91f948d303e1060d4bdd650a9dea9
Pablo Gallina <pablogallinag@gmail.com>
2021-12-16 20:19:08 -0600

    Dispatch login para redux

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -7,14 +7,17 @@ export const googleLoginPop = () => {
     // gracias al thunk redux, este nos hacer uso del dispatch en esta funcion
     return (dispatch) => {
         firebase.auth().signInWithPopup(googleAuthProvider)
-            .then(data =>{ console.log(data) })
+            .then(({ user }) =>{ 
+                // Ejecutando dispatch para guardar los datos del login
+                dispatch(googleLogin( user.uid, user.displayName ))
+            })
     }
 }
 
 // archivo contendra los payloads y types de las actions del auth
-export const googleLogin = (id, username) => {
+export const googleLogin = (uid, displayName) => {
     return {
         type: types.login, 
-        payload:{id, username}
+        payload:{uid, displayName}
     }
 }
