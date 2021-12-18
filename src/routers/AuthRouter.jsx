721adae15ffd78ee1265d8466f59d339720051b6
Pablo Gallina <pablogallinag@gmail.com>
2021-12-17 20:26:55 -0600

    Capturar sesiones iniciadas

--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { useDispatch } from "react-redux";
+
 import LoginScreen from "../pages/LoginScreen"
 import RegisterScreen from "../pages/RegisterScreen"
 
+import { firebase } from '../firebase/config.js';
+import { googleLogin } from "../redux/actions/auth";
+
 const AuthRouter = () => {
+    // ejecutar un dispatch en caso de que haya una sesion iniciada
+    const dispatch = useDispatch()
+
+    // Use effect para verificar si hay un cambio en nuestra aplicacion
+    useEffect(() => {
+        // notificara si el estado de la autenticacion cambio
+        firebase.auth().onAuthStateChanged( user=>{
+            dispatch(googleLogin( user.uid, user.displayName ))
+        })
+    }, [])
     return (
         <Router>
             <Switch>
