8af5b0db96d2583c68bce6655a70a69e40a849d9
Pablo Gallina <pablogallinag@gmail.com>
2021-12-22 18:42:35 -0600

    reducer para app

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import { authReducer } from '../reducers/AuthReducer'
 import thunk from 'redux-thunk';
+import { appReducer } from '../reducers/AppReducer';
 
 // para poder mandar/usar varios reducers
 const reducers = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    app: appReducer
 })
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
