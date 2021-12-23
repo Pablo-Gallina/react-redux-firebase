222e6b25fb42bbfebfaa0805bb908109ad21498a
Pablo Gallina <pablogallinag@gmail.com>
2021-12-22 18:36:44 -0600

    useSelector para capturar el nombre del usuario desde estado redux

--- src/pages/AppScreen.jsx
+++ src/pages/AppScreen.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { logOut } from '../redux/actions/auth';
 
 const AppScreen = () => {
     const dispatch = useDispatch();
+
+    // Retorna el state de redux (auth)
+    const { auth } = useSelector(state => state);
 
     const handleLogOut = () => {
         dispatch(logOut())
@@ -11,6 +14,8 @@ const AppScreen = () => {
     return (
         <>
             <h1>App screen</h1>
+
+            <h1>Hola { auth.displayName }</h1>
             <button onClick={handleLogOut}>LogOut</button>
         </>
     )
